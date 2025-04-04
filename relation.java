d60d7f5ffb3d823c46498f84a48992b1c2404a27
harshita9708 <harshitahosamani04@gmail.com>
2025-04-04 10:44:03 +0530

    save the relation file

---
+++ relation.java
@@ -0,0 +1,19 @@
+public class relation{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        if(a>b){
+        System.out.println("A is greater than B "+(a>b));
+        }
+        else if(a<b){
+        System.out.println("B is greater than A "+(a<b));
+        }
+        else if(a==b){
+        System.out.println("A is equal to  B "+(a==b));
+        }
+        else{
+        System.out.println("A is not equal to B "+(a!=b));
+        }
+    }
+}
